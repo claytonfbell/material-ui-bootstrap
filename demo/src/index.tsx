bcea4939fdc618536bfb3fed6947f54254ae13d2
Clay Bell <clay.bell@etix.com>
2021-10-30 10:29:04 -0700

    chore: tried reordering imports for polyfills

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,6 +1,8 @@
-import React from "react"
+// polyfills must be at the top!
 import "react-app-polyfill/ie11"
 import "react-app-polyfill/stable"
+
+import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 import "./index.css"
