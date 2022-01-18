57cc65df463a9c19e64fb30fe03aac49938f18f2
Clay Bell <clay.bell@etix.com>
2022-01-18 15:08:14 -0800

    fix: Cleanup ts dependency conflict

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -1,46 +1,10 @@
 import Paper from "@mui/material/Paper"
-import { alpha, styled } from "@mui/material/styles"
+import { alpha, useTheme } from "@mui/material/styles"
 import MuiTab, { TabProps as MuiTabProps } from "@mui/material/Tab"
 import MuiTabs, { TabsProps as MuiTabsProps } from "@mui/material/Tabs"
 import * as React from "react"
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
-
-export const StyledTabs = styled((props: MuiTabsProps) => (
-  <MuiTabs {...props} />
-))(() => ({
-  marginBottom: -1,
-  "& .MuiTabs-scroller": {
-    overflow: `visible !important`,
-  },
-  "& .MuiTabs-indicator": {
-    display: "none",
-  },
-}))
-
-const StyledTab = styled((props: MuiTabProps) => (
-  <MuiTab disableRipple {...props} />
-))(({ theme }) => ({
-  textTransform: "none",
-  borderLeft: `1px solid ${alpha(theme.palette.divider, 0)}`,
-  borderRight: `1px solid ${alpha(theme.palette.divider, 0)}`,
-  borderTop: `1px solid ${alpha(theme.palette.divider, 0)}`,
-  color: theme.palette.primary.main,
-  borderTopLeftRadius: 6,
-  borderTopRightRadius: 6,
-  ":hover": {
-    borderLeft: `1px solid ${alpha(theme.palette.primary.main, 0.25)}`,
-    borderRight: `1px solid ${alpha(theme.palette.primary.main, 0.25)}`,
-    borderTop: `1px solid ${alpha(theme.palette.primary.main, 0.25)}`,
-  },
-  "&.Mui-selected": {
-    color: theme.palette.text.secondary,
-    backgroundColor: theme.palette.background.paper,
-    borderLeft: `1px solid ${theme.palette.divider}`,
-    borderRight: `1px solid ${theme.palette.divider}`,
-    borderTop: `1px solid ${theme.palette.divider}`,
-  },
-}))
 
 export type TabsProps = Omit<
   MuiTabsProps,
@@ -56,13 +20,24 @@ export const Tabs = React.forwardRef(function(
   { tabs, children, selectedIndex, onSelect: onSelect, ...props }: TabsProps,
   ref: any
 ) {
+  const theme = useTheme()
+
   return (
     <>
-      <StyledTabs
+      <MuiTabs
         ref={ref}
         {...props}
         value={selectedIndex}
         onChange={(_e, value) => onSelect(value)}
+        sx={{
+          marginBottom: `-1px`,
+          "& .MuiTabs-scroller": {
+            overflow: `visible !important`,
+          },
+          "& .MuiTabs-indicator": {
+            display: "none",
+          },
+        }}
       >
         {tabs
           .map(tab => {
@@ -72,9 +47,44 @@ export const Tabs = React.forwardRef(function(
             return tab
           })
           .map((tab, index) => (
-            <StyledTab key={index} {...tab} value={index} />
+            <MuiTab
+              disableRipple
+              key={index}
+              {...tab}
+              value={index}
+              sx={{
+                textTransform: "none",
+                borderLeft: `1px solid ${alpha(theme.palette.divider, 0)}`,
+                borderRight: `1px solid ${alpha(theme.palette.divider, 0)}`,
+                borderTop: `1px solid ${alpha(theme.palette.divider, 0)}`,
+                color: theme.palette.primary.main,
+                borderTopLeftRadius: 6,
+                borderTopRightRadius: 6,
+                ":hover": {
+                  borderLeft: `1px solid ${alpha(
+                    theme.palette.primary.main,
+                    0.25
+                  )}`,
+                  borderRight: `1px solid ${alpha(
+                    theme.palette.primary.main,
+                    0.25
+                  )}`,
+                  borderTop: `1px solid ${alpha(
+                    theme.palette.primary.main,
+                    0.25
+                  )}`,
+                },
+                "&.Mui-selected": {
+                  color: theme.palette.text.secondary,
+                  backgroundColor: theme.palette.background.paper,
+                  borderLeft: `1px solid ${theme.palette.divider}`,
+                  borderRight: `1px solid ${theme.palette.divider}`,
+                  borderTop: `1px solid ${theme.palette.divider}`,
+                },
+              }}
+            />
           ))}
-      </StyledTabs>
+      </MuiTabs>
       <Paper
         variant="outlined"
         sx={{
